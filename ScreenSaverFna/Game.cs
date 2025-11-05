c43c8f4c3cf582641e9ddbb366a4dc6f6b7796b1
Ilya Kaznacheev <ilkaznachey@gmail.com>
2025-11-05 20:58:12 +0300

    добавлено: бибилиотки в зависимости и подчистка ненужного

---
+++ ScreenSaverFna/Game.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace ScreenSaverFna
+{
+    public class ScreenSaver : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch = null!;
+
+        public ScreenSaver() //This is the constructor, this function is called whenever the game class is created.
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// This function is automatically called when the game launches to initialize any non-graphic variables.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Automatically called when your game launches to load any game assets (graphics, audio etc.)
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        /// <summary>
+        /// Called each frame to update the game. Games usually runs 60 frames per second.
+        /// Each frame the Update function will run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        protected override void Update(GameTime gameTime)
+        {
+            //Update the things FNA handles for us underneath the hood:
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game is ready to draw to the screen, it's also called each frame.
+        /// </summary>
+        protected override void Draw(GameTime gameTime)
+        {
+            //This will clear what's on the screen each frame, if we don't clear the screen will look like a mess:
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            //Draw the things FNA handles for us underneath the hood:
+            base.Draw(gameTime);
+        }
+    }
+}
--- ScreenSaverFna/Program.cs
+++ ScreenSaverFna/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Xna.Framework;
+
+namespace ScreenSaverFna
+{
+    static class Program
+    {
+        /// <summary>
+        /// Точка входа в пограмму
+        /// </summary>
+        static void Main()
+        {
+            using var screenSaver = new ScreenSaver();
+            screenSaver.Run();
+        }
+    }
+}
